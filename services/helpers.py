c5b117f512465f79be16c1304d4db5b3794556c0
Joseph Kim <josek16@uci.edu>
2025-02-01 13:17:36 -0800

    add: get player in turn helper

--- services/helpers.py
+++ services/helpers.py
@@ -8,5 +8,9 @@ def get_seats(game_state):
     return seats
 
 
+def get_player_in_turn(game_state):
+    return game_state['pITT']
+
+
 def is_hand_over(game_state):
     return 'gameResult' in game_state and game_state['gameResult'] != '<D>'

5a551c1700fc03a3674d38ba4f3942c627fc903c
Joseph Kim <josek16@uci.edu>
2025-02-01 19:13:23 -0800

    add: member variables for player model

--- models/player.py
+++ models/player.py
@@ -2,6 +2,11 @@ class Player:
     def __init__(self, id, stack):
         self.id = id
         self.stack = stack
+        self.hand_count = 0
+        self.vpip = 0
+        self.pfr = 0
+        self.three_bet = 0
+        self.c_bet = 0
 
     def update_stack(self, stack):
         self.stack = stack

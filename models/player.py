17033360a3878cbe1986b14c87b25b97d8a6f422
Joseph Kim <josek16@uci.edu>
2025-02-17 19:01:34 -0800

    add: helper functions for increment counter variables

--- models/player.py
+++ models/player.py
@@ -4,10 +4,25 @@ class Player:
 
         self.stack = 0
         self.hand_count = 0
-        self.vpip = 0
-        self.pfr = 0
-        self.three_bet = 0
-        self.c_bet = 0
+        self.vpip_count = 0
+        self.pfr_count = 0
+        self.three_bet_count = 0
+        self.c_bet_count = 0
 
     def update_stack(self, stack):
         self.stack = stack
+
+    def increment_hand_count(self):
+        self.hand_count += 1
+
+    def increment_vpip_count(self):
+        self.vpip_count += 1
+
+    def increment_pfr_count(self):
+        self.pfr_count += 1
+
+    def increment_three_bet_count(self):
+        self.three_bet_count += 1
+
+    def increment_c_bet_count(self):
+        self.c_bet_count += 1

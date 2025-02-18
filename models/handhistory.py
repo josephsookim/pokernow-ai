656748bf5781c5ce6c442c9d163b5b5e6d991b4a
Joseph Kim <josek16@uci.edu>
2025-02-17 18:59:27 -0800

    update: get history function to __str__

--- models/handhistory.py
+++ models/handhistory.py
@@ -2,11 +2,11 @@ class HandHistory:
     def __init__(self):
         self.history = []
 
+    def __str__(self):
+        return '\n'.join(self.history)
+
     def reset(self):
         self.history = []
 
     def add_action(self, action):
         self.history.append(action)
-
-    def get_history(self):
-        return '\n'.join(self.history)
